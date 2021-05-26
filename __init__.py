afb56b320b5a702e40234d8c5757607a4d7cd373
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-05-26 18:44:55 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
+bot_token = os.environ.get('BOT_TOKEN')
+
+tg_client = TelegramClient('bot1', api_id, api_hash).start(bot_token=bot_token)
